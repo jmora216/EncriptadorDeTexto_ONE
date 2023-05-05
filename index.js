ad37282cde70b39077bdb79c3b5d606ec7a8dfdb
Juliana Mora López <jmora216@unicauca.edu.co>
2023-05-05 16:16:46 -0500

    Mejora en alertas de información y error y actualización de documentación.
    Se han mejorado las alertas de información y error utilizando la librería PNotify, con un estilo personalizado y duren solo uno o dos segundos.

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
+// Seleccionar elementos del DOM
 let btnEncrypt = document.querySelector(".btn-encrypt");
 let btnDecrypt = document.querySelector(".btn-decrypt");
-let munieco = document.querySelector(".encrypter-img");
+let icon = document.querySelector(".encrypter-img");
 let container = document.querySelector(".encrypted-msg");
 let result = document.querySelector(".output-text");
 const textEncrypt = document.querySelector(".textarea-encrypt");
 
-
+// Cifrar el texto cuando se hace clic en el botón de encriptar
 btnEncrypt.onclick = function () {
-    hideElements();
     const text = getText();
     if (text.length > 0) {
+        hideElements();
         result.textContent = encryptText(text);
     } else {
-        showAlert();
-        munieco.classList.remove("hide");
+        icon.classList.remove("hide");
         container.classList.remove("hide");
         result.textContent = "";
     }
 };
 
+// Descifrar el texto cuando se hace clic en el botón de desencriptar
 btnDecrypt.onclick = function () {
-    hideElements();
     const text = getText();
     if (text.length > 0) {
+        hideElements();
         result.textContent = decryptText(text);
     } else {
-        showAlert();
-        munieco.classList.remove("hide");
+        icon.classList.remove("hide");
         container.classList.remove("hide");
         result.textContent = "";
     }
 };
 
+/**
+Obtener el texto de entrada, eliminar caracteres especiales y acentos.
+Mostrar alerta cuando la entrada contiene letras invalidas o cuando está vacía.
+@return {string} - El texto de entrada.
+**/
 function getText() {
     const text = document.querySelector(".textarea-encrypt");
     let value = text.value.trim();
 
-    // Reemplazar caracteres especiales y acentos
-    value = value.replace(/[^\w\s]/gi, '').normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    if (value.length === 0) {
+        PNotify.notice({
+            title: "Ooops!",
+            text: "Por favor ingrese un texto.",
+            delay: 1000,
+            addClass: 'my-pnotify-style'
+        });
+        return "";
+    }
 
     if (!/^[a-z\s]+$/.test(value)) {
-        swal("Ooops!", "Ingrese solo letras minúsculas y sin acentos", "info");
+        PNotify.error({
+            title: "Ooops!",
+            text: "Ingrese solamente letras minúsculas y sin acentos.",
+            delay: 2000,
+            addClass: 'my-pnotify-style'
+        });
         value = "";
         text.value = value;
         value = text.value.trim();
@@ -49,16 +66,24 @@ function getText() {
     return value;
 }
 
+
+// Convertir el texto ingresado a minúsculas
 textEncrypt.addEventListener("input", function () {
     const value = this.value;
     this.value = value.toLowerCase();
 });
 
+// Ocultar algunos elementos del DOM
 function hideElements() {
-    munieco.classList.add("hide");
+    icon.classList.add("hide");
     container.classList.add("hide");
 }
 
+/**
+Encriptar el texto de entrada
+@param {string} msg - El texto de entrada a encriptar.
+@return {string} - El texto encriptado.
+**/
 function encryptText(msg) {
     const finalText = msg
         .replace(/e/gi, "enter")
@@ -70,11 +95,14 @@ function encryptText(msg) {
     if (finalText.length > 0) {
         textEncrypt.value = "";
         return finalText;
-    } else {
-        showAlert();
     }
 }
 
+/**
+Desencriptar el texto de entrada
+@param {string} msg - El texto de entrada a desencriptar.
+@return {string} - El texto desencriptado.
+**/
 function decryptText(msg) {
     const finalText = msg
         .replace(/enter/gi, "e")
@@ -86,25 +114,35 @@ function decryptText(msg) {
     if (finalText.length > 0) {
         textEncrypt.value = "";
         return finalText;
-    } else {
-        showAlert();
     }
 }
 
-function showAlert() {
-    swal("Ooops!", "Debes ingresar un texto \n Ingrese solo letras minúsculas y sin acentos", "info");
-}
-
+// Obtener el contenido del texto de salida
 const btnCopy = document.querySelector(".btn-copy");
 btnCopy.onclick = function () {
     const content = result.textContent.trim();
     if (content.length > 0) {
         navigator.clipboard.writeText(content).then(() => {
-            swal("Copiado!", "El texto ha sido copiado al portapapeles", "success");
+            PNotify.success({
+                title: "Copiado!",
+                text: "El texto ha sido copiado al portapapeles",
+                delay: 1000,
+                addClass: 'my-pnotify-style'
+            });
         }).catch(() => {
-            swal("Oops!", "No se pudo copiar el texto", "error");
+            PNotify.error({
+                title: "Oops!",
+                text: "No se pudo copiar el texto",
+                delay: 1000,
+                addClass: 'my-pnotify-style',
+            });
         });
     } else {
-        swal("Ooops!", "No hay texto para copiar", "info");
+        PNotify.notice({
+            title: "Ooops!",
+            text: "No hay texto para copiar",
+            delay: 1000,
+            addClass: 'my-pnotify-style'
+        });
     }
 };
